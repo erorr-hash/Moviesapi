3fe2cebc49fdabc7816c6eadba552c0bfc6a1cef
erorr_hash <22160106@su.edu.ye>
2025-01-29 00:42:56 +0300

    add [Post] Create Movie  Endpoint

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Moviesapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private List<string> _allowedExtenstions = new List<string> { ".jpg",".png"};
+        private long _maxAllowedPosterSize = 5242880;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromForm]MovieDto dto)
+        {
+            if(!_allowedExtenstions.Contains(Path.GetExtension(dto.Poster.FileName.ToLower()))) 
+                return BadRequest("only jpg and png images are allowed!");
+
+            if (dto.Poster.Length > _maxAllowedPosterSize)
+                return BadRequest("max allowed size is 5MB!");
+
+            var isValidGenre = await _context.Genres.AnyAsync(g => g.Id == dto.GenreId);
+            if (!isValidGenre)
+            {
+                return BadRequest("invalid genre id!");
+            }
+            
+            using var dataStream = new MemoryStream();
+            await dto.Poster.CopyToAsync(dataStream);
+            var movie = new Movie()
+            {
+                GenreId = dto.GenreId,
+                Title = dto.Title,
+                Poster = dataStream.ToArray(),
+                Rate = dto.Rate,
+                Storeline = dto.Storeline,
+                Year = dto.Year,
+            };
+            await _context.AddAsync(movie);
+            _context.SaveChangesAsync();
+            return (Ok(movie));
+            
+
+        }
+    }
+}
--- Dtos/MovieDto.cs
+++ Dtos/MovieDto.cs
@@ -1,19 +1,18 @@
-﻿namespace Moviesapi.Models
+﻿namespace Moviesapi.Dtos
 {
-    public class Movie
+    public class MovieDto
     {
-        public int Id { get; set; }
         [MaxLength(255)]
         public string Title { get; set; }
 
         public int Year { get; set; }
 
         public double Rate { get; set; }
-        [MaxLength (255)]
+        [MaxLength(255)]
         public string Storeline { get; set; }
 
-        public byte[] Poster { get; set; }
+
+        public IFormFile Poster { get; set; }
         public byte GenreId { get; set; }
-        public Genre Genre { get; set; }
     }
 }
